c0ceaed1f2e973a2a5f3bb8b66e86204ca073218
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-05-06 15:41:56 +0545

    API call using Volley library

---
+++ app/src/main/java/com/chaudharynabin6/memeshare/INFO.md
@@ -0,0 +1,8 @@
+# API call
+## use internet permission
+* https://stackoverflow.com/questions/2169294/how-to-add-manifest-permission-to-an-application
+* https://developer.android.com/reference/android/Manifest.permission#INTERNET
+
+## use Volley Library
+* https://google.github.io/volley/request.html
+  
--- app/src/main/java/com/chaudharynabin6/memeshare/MainActivity.kt
+++ app/src/main/java/com/chaudharynabin6/memeshare/MainActivity.kt
@@ -2,6 +2,14 @@ package com.chaudharynabin6.memeshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.ASSERT
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.chaudharynabin6.memeshare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +20,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(this.binding.root)
 
 //        this.binding.memeImageView.setImageResource(R.mipmap.ic_launcher)
+
+
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                Log.w("json response",response.toString(1))
+                Log.w("title",response.getString("title"))
+            },
+            Response.ErrorListener { error ->
+                // TODO: Handle error
+                Log.w("json error",error.localizedMessage)
+            }
+        )
+
+    // Add the request to the RequestQueue.
+            queue.add(jsonObjectRequest)
     }
+//   for network call use https://google.github.io/volley/
 }
