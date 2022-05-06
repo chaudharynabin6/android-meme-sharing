9ef5bf1eecd11d79ac6f67c923206d94daebd60d
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-05-06 22:07:02 +0545

    Share Button : Functionality image

--- app/src/main/java/com/chaudharynabin6/memeshare/INFO.md
+++ app/src/main/java/com/chaudharynabin6/memeshare/INFO.md
@@ -9,3 +9,11 @@
 ## using glide for image Loading and Caching from network
 * https://github.com/bumptech/glide
 * https://ourcodeworld.com/articles/read/929/top-10-best-android-image-loading-and-caching-libraries
+
+
+## sharing image 
+    * source: https://www.geeksforgeeks.org/how-to-share-image-of-your-app-with-another-app-in-android/
+    * fixing permission error:https://errorsfixing.com/permission-denial-while-sharing-file-with-fileprovider/
+
+## sharing different types
+    * https://guides.codepath.com/android/Sharing-Content-with-Intents
--- app/src/main/java/com/chaudharynabin6/memeshare/MainActivity.kt
+++ app/src/main/java/com/chaudharynabin6/memeshare/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.chaudharynabin6.memeshare
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.util.Log.ASSERT
 import android.view.View
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.core.graphics.drawable.toBitmap
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -19,7 +21,8 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.chaudharynabin6.memeshare.databinding.ActivityMainBinding
-
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -86,15 +89,70 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun shareMeme(view: View) {
-        val sendIntent: Intent = Intent().apply {
-            action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, this@MainActivity.currentUrl)
-            type = "text/plain"
+//        val sendIntent: Intent = Intent().apply {
+//            action = Intent.ACTION_SEND
+//            putExtra(Intent.EXTRA_TEXT, this@MainActivity.currentUrl)
+//            type = "text/plain"
+//        }
+//
+//        val shareIntent = Intent.createChooser(sendIntent, "Share Image URL")
+//        startActivity(shareIntent)
+        val bitmap = this.binding.memeImageView.drawable.toBitmap()
+        shareImageAndText(bitmap)
+    }
+
+    private fun shareImageAndText(bitmap: Bitmap) {
+        val uri: Uri? = getImageToShare(bitmap)
+        val intent = Intent(Intent.ACTION_SEND)
+
+        // putting uri of image to be shared
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+
+        // adding text to share
+        intent.putExtra(Intent.EXTRA_TEXT, "Sharing Image")
+
+        // Add subject Here
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here")
+
+        // setting type to image
+        intent.type = "image/png"
+
+        // calling startactivity() to share
+//        startActivity(Intent.createChooser(intent, "Share Via"))
+
+        val chooser = Intent.createChooser(intent, "Share File")
+
+        val resInfoList =
+            this.packageManager.queryIntentActivities(chooser, PackageManager.MATCH_DEFAULT_ONLY)
+
+        for (resolveInfo in resInfoList) {
+            val packageName = resolveInfo.activityInfo.packageName
+            grantUriPermission(
+                packageName,
+                uri,
+                Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
         }
 
-        val shareIntent = Intent.createChooser(sendIntent, "Share Image URL")
-        startActivity(shareIntent)
+        startActivity(chooser)
     }
 
 
+    // Retrieving the url to share
+    private fun getImageToShare(bitmap: Bitmap): Uri? {
+        val imageFolder = File(cacheDir, "images")
+        var uri: Uri? = null
+        try {
+            imageFolder.mkdirs()
+            val file = File(imageFolder, "shared_image.png")
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            uri = FileProvider.getUriForFile(this, "com.chaudharynabin6.memeshare.fileprovider", file)
+        } catch (e: Exception) {
+            Toast.makeText(this, "" + e.message, Toast.LENGTH_LONG).show()
+        }
+        return uri
+    }
 }
